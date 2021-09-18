f310ee32a35168c431cbe1649e9a4a3ad009f029
wingsmight <wingsmight@gmail.com>
2021-09-18 18:25:39 +0700

    Initial Commit

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Igoryok on 18.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
