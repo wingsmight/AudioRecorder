d39913bf57a793fcfde40233705368fad27a4256
wingsmight <wingsmight@gmail.com>
2022-01-14 12:46:16 +0700

    add app description to app info section

--- AudioRecorder/Views/SettingsTab/AppInfoView.swift
+++ AudioRecorder/Views/SettingsTab/AppInfoView.swift
@@ -11,17 +11,21 @@ struct AppInfoView: View {
     @Binding var isShowing: Bool
     
     let text = """
-    What is Lorem Ipsum?
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Данное приложение представляет собой продвинутый диктофон, предназначенный для обеспечения безопасности владельца устройства и предотвращения бытовых и криминальных конфликтов. Включенное в фоновом режиме, приложение автоматически включает запись, распознав в пределах слышимости человеческую речь, записывая небольшие аудиофайлы до трех минут и сохраняя их в память телефона и облачное хранилище. После того, как человеческая речь стихает, приложение вновь переходит в режим ожидания.
 
-    Why do we use it?
-    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+Иногда, во время возникновения конфликтов, случается такое, что нет никакой возможности достать из кармана телефон и начать фиксировать происходящее на диктофон или видеокамеру:
+- незнакомец предлагает ребенку показать леденцы в затонированном фургоне;
+- в темной подворотне трое просят закурить и поделиться деньгами;
+- пассажиры невежливо просят таксиста выйти из автомобиля и дать им порулить...
+Одно лишь упоминание о том, что в данный момент ведется аудиозапись происходящего напрямую в облачное хранилище, может вывести назревающий конфликт из горячей фазы или вообще свести его на нет.
+В крайнем же случае, сотрудники правоохранительных органов могут получить записи из облака для оценки произошедшего и установления причин и виновников конфликта.
 
+Предоставив приложению доступ к геолокации, в списке аудиофайлов будет видно, где находился телефон во время осуществления записи.
 
-    Where does it come from?
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+В памяти устройства может храниться не более 50 записей. При достижении лимита, новые записи будут сохраняться после удаления самых старых файлов.
+Размер облака составляет 200 MB, но при желании его можно расширить.
 
-    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+Берегите себя!
 """
     
     
